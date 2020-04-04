e3902507816b631ea4aaf4ff7563aa51e3965bc1
Aleksandras Zagorskas <aleksandras.zagorskas@gmail.com>
2020-04-04 11:47:42 +0300

    Babylon test scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PageWithScene from './components/PageWithScene';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <PageWithScene/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello world
@@ -18,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
